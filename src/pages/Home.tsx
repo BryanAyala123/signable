6ef4a3fbbb68627f753ff9651bafe3d86f1ddccf
Adrian Rojas <rojasa@bu.edu>
2025-10-06 02:52:14 -0400

    added a personal touch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Signable</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>

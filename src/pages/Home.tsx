5e9697cda738c0a4d4ce479ef38fc8b245cee1b4
BryanAyala123 <bayala@bu.edu>
2025-11-02 22:28:42 -0500

    creatde the home page based on design ask

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,17 @@ import AppFooter from '../components/layout/AppFooter';
 import { useAuth } from '../contexts/authContext';
 import { doSignOut } from '../firebase/auth';
 import { useIonRouter } from '@ionic/react';
+import MtSetTable from '../components/MtSetTable/MtSetTable';
+import Notebook from '../components/Notebook/Notebook';
+import ChatBot from '../components/Chatbot/Chatbot';
+import eyeball from '/public/assets/eyeball.svg';
+import computer from '/public/assets/computer.svg';
+import squareCheck from '/public/assets/square-check.svg';
+import squareQuestion from '/public/assets/square-question.svg';
+import lessThan from '/public/assets/lessThan.svg';
+import moreThan from '/public/assets/moreThan.svg';
+import phone from '/public/assets/phone.svg';
+import checker from '/public/assets/checker.svg';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -26,56 +37,92 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen className="ion-padding">
         <div className="home-content">
-          <h1>Welcome to Signable: {currentUser?.displayName}!</h1>
-          <h1>Practice ASL with Flash Cards</h1>
-          <p>Practice American Sign Language with interactive flash cards</p>
-          
-          <IonButton 
-            expand="block" 
-            onClick={() => history.push('/flashcards/alphabet')}
-          >
-            Start Practicing
-          </IonButton>
-        </div>
-        
-        <div className="home-content">
-          <h1>Sign Your Way to Victory!</h1>
-          <p>Practice ASL the Fun Way — One Game at a Time!</p>
-          
-          <IonButton 
-            expand="block" 
-            onClick={() => history.push('/games')}
-          >
-            Start Playing
-          </IonButton>
-        </div>
+          <div className='homeContentLeftSide'>
+            <MtSetTable />
+            <div className='homeContentLeftSideStudy'>
+              <div className='homeContentLeftSideStudyHeader'>
+                <p><u>Study</u></p>
+              </div>
+              <div className='homeContentLeftSideStudyContent'>
+                <button onClick={() => history.push('/slr')}>
+                  <div className='homeContentLeftSideStudyContentSLR'>
+                    <p className='homeContentLeftSideStudyContentSLRText'>ASL Recognition</p>
+                    <img className='eyeballImg'  src={eyeball}/>
+                    <img className='computerImg'  src={computer}/>
+                  </div>
+                </button>
+                <button onClick={() => history.push('/games')}>
+                  <div className='homeContentLeftSideStudyContentMemory'>
+                    <p className='homeContentLeftSideStudyContentMemText'>Memory</p>
+                    <div className='homeContentLeftSideStudyContentMemoryImages'>
+                      <img src={squareQuestion} className='homeContentLeftSideStudyContentMemoryImg'/>
+                      <img src={squareQuestion} className='homeContentLeftSideStudyContentMemoryImg'/>
+                      <img src={squareCheck} className='homeContentLeftSideStudyContentMemoryImg'/>
+                    </div>
+                    <div className='homeContentLeftSideStudyContentMemoryImages'>
+                      <img src={squareCheck} className='homeContentLeftSideStudyContentMemoryImg'/>
+                      <img src={squareQuestion} className='homeContentLeftSideStudyContentMemoryImg'/>
+                      <img src={squareQuestion} className='homeContentLeftSideStudyContentMemoryImg'/>
+                    </div>
+                  </div>
+                </button>
+                <button onClick={() => history.push('/flashcards/alphabet')}>
+                  <div className='homeContentLeftSideStudyContentFlashcard'>
+                    <p className='homeContentLeftSideStudyContentFlashText'>Flashcards</p>
+                    <div className='homeContentLeftSideStudyContentFlashcardInnerDiv'>
+                      <img src={lessThan} className='homeContentLeftSideStudyContentFlashcardInnerDivImg'/>
+                      <img src={phone} className='homeContentLeftSideStudyContentFlashcardInnerDivImgPhone'/>
+                      <img src={moreThan} className='homeContentLeftSideStudyContentFlashcardInnerDivImg'/>
+                    </div>
+                    <img src={checker} className='homeContentLeftSideStudyContentFlashcardChecker'/>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
 
-        <div className="home-content">
-          <h1>Sign</h1>
-          <p>Practice ASL</p>
-          
-          <IonButton 
-            expand="block" 
-            onClick={() => history.push('/slr')}
-          >
-            Start Playing
-          </IonButton>
-        </div>
+          <div className='homeContentRightSide'>
+            <div className='homeRightSideTopDiv'>
+              <div className='homeContentRightSideNameDiv'>
+                <div className='MainHeaderText'>
+                  <h1 className='MainHeaderTextName'>Hi, {currentUser?.displayName}!</h1>
+                </div>
+                <div className='MainHeaderStreak'>
+                  <p><span>7</span>/8 Terms Memorized Today</p>
+                </div>
+                <div className='ViewGoals'>
+                  <u>View Daily Goals</u>
+                </div>
+              </div>
 
-        <div className="home-content">
-          <h1>Take Notes!</h1>
-          <p>Get the edge on your learning with video notes.</p>
+              <div className='MessageBoard'>
+                <div className='MessageBoardOuterDiv'>
+                  <div className='MessageBoardInnerDiv'>
+                    <p className='AmountMessages'>5</p>
+                  </div>
+                  <p className='MessagesLink'><u>Messages</u></p>
+                </div>
+              </div>
+            </div>
+            <div className='homeRightSideBottomDiv'>
+              <div className='homeContentNotebook'>
+                <div className='homeContentNotebookHeader'>
+                  <p><u>Notebook</u></p>
+                </div>
+                <div className='NotebookContent'>
+                  <Notebook />
+                </div>
+              </div>
 
-          <IonButton
-            expand="block"
-            onClick={() => history.push('/notes')}
-          >
-            Start Notetaking
-          </IonButton>
+              <div className='homeContentChatbot'>
+                <div className='homeContentChatBotHeader'>
+                  <p><u>Chat with Signaldo</u></p>
+                  </div>
+                  <ChatBot miniMode={true}/>
+              </div>
+            </div>
+          </div>
         </div>
-        <IonButton expand="block" onClick={handleLogout}>
-            Log Out
-          </IonButton>
       </IonContent>
       <AppFooter />
     </IonPage>
